9ee3a47f49c30be4507eb8982b6e535f8efaa64c
9kwit9 <kwit.lis@gmail.com>
2023-11-08 11:13:56 +0700

    modified:   MVCProject/test.py

--- MVCProject/test.py
+++ MVCProject/test.py
@@ -2,3 +2,9 @@ print("Suwit wangsanam")
 print("Suwit wangsanam")
 print("Suwit wangsanam")
 print("Suwit wangsanam")
+
+
+print("Suwit wangsanam")
+print("Suwit wangsanam")
+print("Suwit wangsanam")
+print("Suwit wangsanam")
